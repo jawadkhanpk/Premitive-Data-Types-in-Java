cda9890c0debe3831b9bcd6342a6a1731a425b2f
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-08-06 12:34:51 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // premitive datatype
+        int a = 13;
+        float b = 0.4f;
+        char c = 'x';
+        boolean d = true;
+        short e = 23;
+        long f = 434;
+        double g = 12.3;
+        byte h = 10;
+
+        System.out.println("The Value of A Integer is: " +a);
+        System.out.println("The Value of B Float is: " +b);
+        System.out.println("The Value of C Char is:" +c);
+        System.out.println("The Value of D Boolean is:" +d);
+        System.out.println("The Value of E Short  is:" +e);
+        System.out.println("The Value of F Long is:" +f);
+        System.out.println("The Value of G Double is:" +g);
+        System.out.println("The Value of H Byte is:" +h);
+
+        
+    }
+}
